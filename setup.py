08371909e5bd5c3b70a071038c140a7b61649015
vpanayot <valentin.panayotov1995@gmail.com>
2026-01-21 14:36:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="dab_project",
     version="0.0.1",
-    packages=find_pakcages(where="."),
+    packages=find_packages(where="."),
     package_dir={"":"."},
     install_requires=["setuptools"]
 )
