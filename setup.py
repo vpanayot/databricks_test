782e8c3003d8a388e2bcabe29526130913b83cb1
vpanayot <valentin.panayotov1995@gmail.com>
2026-01-21 14:35:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="dab_project",
     version="0.0.1",
     packages=find_pakcages(where="."),
-    package_dir={"":"."}
+    package_dir={"":"."},
     install_requires=["setuptools"]
 )
