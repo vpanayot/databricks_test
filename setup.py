dae19f0c7005ca0c6d2865673b51ff44bfba9f2f
vpanayot <valentin.panayotov1995@gmail.com>
2026-01-21 14:33:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dab_project",
+    version="0.0.1",
+    packages=find_pakcages(where="."),
+    package_dir={"":"."}
+    install_requires=["setuptools"]
+)
